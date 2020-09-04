2a0aee2cc10484a0bc7b9ffdfee5dc6a2e05a5f4
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-04 12:19:10 +0200

    [FEATURE][HOME] * Table view was added

--- SwiftUI_22_Realm/Views/ViewController.swift
+++ SwiftUI_22_Realm/Views/ViewController.swift
@@ -2,7 +2,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let reusableCellId = "userCell"
+    
+    @IBOutlet weak var userTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -11,3 +15,17 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reusableCellId, for: indexPath)
+        cell.textLabel?.text = "Titulo de la celd"
+        cell.detailTextLabel?.text = "subtitulo de la celda"
+        return cell
+    }
+    
+    
+}
